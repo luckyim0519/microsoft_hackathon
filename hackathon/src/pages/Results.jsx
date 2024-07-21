c397850148f56938d4c370491942c62d16f81659
jenna5376 <jenna5376@gmail.com>
2024-07-21 09:04:56 -0700

    added preloader

--- hackathon/src/pages/Results.jsx
+++ hackathon/src/pages/Results.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import MapComponent from './map'; // Adjust the path as needed
 import OpenAI from "openai";
+import Lottie from "react-lottie";
+import preloader from "../assets/preloader.json";
+
+
 
 function Results() {
   const { state: { response } = {} } = useLocation();
@@ -9,6 +13,15 @@ function Results() {
   const [history, setHistory] = useState("");
   const [recommendations, setRecommendations] = useState([]);
   const [finalLocations, setFinalLocations] = useState([]);
+  
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: preloader,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   useEffect(() => {
     const openai = new OpenAI({ apiKey: "sk-None-LzkASTDUaEkaIMYZdKhDT3BlbkFJWGAGvI4TnJSw2hZiL1qi", dangerouslyAllowBrowser: true });
@@ -57,31 +70,42 @@ function Results() {
   }, [response.destination, response.activity, response.vibe, response.budget]);
 
   return (
-    <div>
-      <p>This trip is powered by AI</p>
-      <h1>Your trip to {response.destination} includes...</h1>
-      <p>Activities: {response.activity.join(', ')}</p>
-      <p>Vibe: {response.vibe.join(", ")}</p>
-      <p>Budget: {response.budget.join(", ")}</p>
-      <h2>Recommendations:</h2>
-      {recommendations.map((recommendation, index) => (
-        <div key={index}>
-          <RecommendationDetails recommendation={recommendation} />
+    <div className='h-screen max-h-svh min-h-svh'>
+      {itinerary.length == 0 ? (
+        <div className="flex flex-col gap-8 text-center m-auto pt-48">
+          <h1 className="text-2xl font-semibold">
+            AI is planning your itinerary
+          </h1>
+          <Lottie options={defaultOptions} width={500} />
         </div>
-      ))}
-      <h2>History:</h2>
-      <p>{history}</p>
-      <h2>Itinerary:</h2>
-      <p>{itinerary}</p>
-      {finalLocations.length > 0 && (
+      ) : (
         <div>
-          <h2>Recommended Locations:</h2>
-          {finalLocations.map((location, index) => (
+          <h2>Recommendations:</h2>
+          <p>This trip is powered by AI</p>
+          <h1>Your trip to {response.destination} includes...</h1>
+          <p>Activities: {response.activity.join(", ")}</p>
+          <p>Vibe: {response.vibe.join(", ")}</p>
+          <p>Budget: {response.budget.join(", ")}</p>
+          {recommendations.map((recommendation, index) => (
             <div key={index}>
-              <LocationDetails location={location} />
+              <RecommendationDetails recommendation={recommendation} />
             </div>
           ))}
-          <MapComponent google={window.google} locations={finalLocations} />
+          <h2>History:</h2>
+          <p>{history}</p>
+          <h2>Itinerary:</h2>
+          <p>{itinerary}</p>
+          {finalLocations.length > 0 && (
+            <div>
+              <h2>Recommended Locations:</h2>
+              {finalLocations.map((location, index) => (
+                <div key={index}>
+                  <LocationDetails location={location} />
+                </div>
+              ))}
+              <MapComponent google={window.google} locations={finalLocations} />
+            </div>
+          )}
         </div>
       )}
     </div>
