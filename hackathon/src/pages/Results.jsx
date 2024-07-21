9edc9acc5e82fbef4cbb788d8ec44b0e83aa2a1d
Lucky Im <84313518+luckyim0519@users.noreply.github.com>
2024-07-21 03:06:27 -0700

    auto-listing

--- hackathon/src/pages/Results.jsx
+++ hackathon/src/pages/Results.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import MapComponent from './map'; // Adjust the path as needed
 
 import OpenAI from "openai";
