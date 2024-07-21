4c6a9306bfcf2a0729cec87dd19122958d947cb8
Jenna (Ju Hee) Han <jenna5376@gmail.com>
2024-07-20 20:53:08 -0700

    set up ant, react router

--- hackathon/src/main.jsx
+++ hackathon/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Form from "./pages/Form.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+          <Route path="/">
+            <Form />
+          </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
